f6fbdb0387638bf82a7b33e72597a5b7693fb2c4
Abeinevincent <abeinevincent@gmail.com>
2023-01-03 20:44:46 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <p>
+          React app built using typescript and with a ci/cd pipeline using github actions
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
